770e5cac2bfad2eb48fd61f94a7e08aeab06a2ac
Wanderpsc <wanderpsc@gmail.com>
2026-02-25 22:25:38 -0300

    chore: point frontend to render backend

--- frontend/config.js
+++ frontend/config.js
@@ -1,2 +1,2 @@
-window.LFN_API_BASES = ['https://lfnapi20260225.loca.lt', 'http://localhost:3333'];
+window.LFN_API_BASES = ['https://lfn-backend.onrender.com', 'http://localhost:3333'];
 window.LFN_API_BASE = window.LFN_API_BASES[0];
